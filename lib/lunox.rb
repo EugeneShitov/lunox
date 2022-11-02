7a4387f812b27cea4f1619bfb632123b56fa1264
Evgeniy Sh <fured.rb@gmail.com>
2022-11-02 20:43:20 +0200

    Gem full update

--- lib/lunox.rb
+++ lib/lunox.rb
@@ -1,33 +1,24 @@
-require 'nokogiri'
-class CreateHTML
-  def create_html(content, bypass_html, file_name: 'index.html')
-    markup = content.gsub!(/[<>]/, '') if bypass_html == false
-    markup = content unless bypass_html == false
-    html = <<~HTML
-      <!doctype html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <title>#{file_name}</title>
-      </head>
-      <body>
-          #{markup}
-      </body>
-      </html>
-    HTML
-    File.write(file_name, html)
-  end
+require 'sanitize'
 
-  def update_html(content, file_name: 'index.html')
-    doc = File.open(file_name) { |f| Nokogiri::HTML(f) }
-    doc.at('body') << content
-
-    file = File.open(file_name, 'w+')
-    file.puts doc
-    file.close
-  end
-
-  def open_in_browser(file_name: 'index.html')
-    system("open ./#{file_name}")
+class Lunox
+  def self.save(body, file_name: 'index.html', bypass_html: false)
+    body = Sanitize.fragment(body) unless bypass_html
+    File.open(file_name, 'w+') do |f|
+      html = <<~HTML
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>#{file_name}</title>
+        </head>
+        <body>
+            <div class="info">
+                #{body}
+            </div>
+        </body>
+        </html>
+      HTML
+      f.write(html)
+    end
   end
 end
--- lunox.gemspec
+++ lunox.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'lunox'
-  s.version     = '0.1.0'
+  s.version     = '1.0.0'
   s.date        = '2022-10-25'
   s.summary     = "Make html file"
   s.description = "Simple html maker"
